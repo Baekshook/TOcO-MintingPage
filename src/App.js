314cadb9da3d8bb93c55858da8f2f9a8e3f3ea85
hoon2 <sanghoon.engineer@gmail.com>
2022-10-29 11:26:32 +0900

    chore: add absolute path alias

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import TestApp from "@components/TestApp";
 import GlobalStyle from "./GlobalStyle";
 
 function App() {
   return (
     <>
       <GlobalStyle />
+      <TestApp />
       <div>안녕</div>
     </>
   );
---
+++ src/components/TestApp.jsx
@@ -0,0 +1,3 @@
+export default function TestApp() {
+  return <div>안녕</div>;
+}
