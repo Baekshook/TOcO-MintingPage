e47679bfd1bb2b2822ddb83e2a909f787d5ec20f
hoon2 <sanghoon.engineer@gmail.com>
2022-11-06 19:45:34 +0900

    랜딩 페이지 초기화 코드

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import GlobalStyle from "./GlobalStyle";
-import styled from "styled-components";
+import { ChakraProvider } from "@chakra-ui/react";
+import Hero from "@components/Hero";
+import NavBar from "@components/NavBar";
+import Features from "@components/Features";
+import Statistics from "@components/Statistics";
+import HeroTwo from "@components/HeroTwo";
+import Footer from "@components/Footer";
+import ThreeTierPricing from "@components/Pricing";
 function App() {
   return (
-    <>
-      <GlobalStyle />
-    </>
+    <ChakraProvider>
+      <NavBar />
+      <Hero />
+      <Features />
+      <Statistics />
+      <ThreeTierPricing />
+      <HeroTwo />
+      <Footer />
+    </ChakraProvider>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,11 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-if (process.env.NODE_ENV === "development") {
-  const { worker } = require("./mock/browser");
-  worker.start({ onUnhandledRequest: "bypass" });
-}
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
