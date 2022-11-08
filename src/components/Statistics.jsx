a7d68945ba4928e24b16d7dbeca5d2b7c75e1e1d
Baekshook <ecgo0106@naver.com>
2022-11-08 11:38:00 +0900

    statistics 개발완료

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -6,7 +6,7 @@ import {
   Stat,
   StatLabel,
   StatNumber,
-  useColorModeValue,
+  Text,
 } from "@chakra-ui/react";
 
 import { BsPerson } from "react-icons/bs";
@@ -20,24 +20,20 @@ function StatsCard(props) {
       px={{ base: 2, md: 4 }}
       py={"5"}
       shadow={"xl"}
-      border={"1px solid"}
-      borderColor={useColorModeValue("gray.800", "gray.500")}
+      border={"3px solid"}
+      borderColor={"purple.700"}
       rounded={"lg"}
     >
       <Flex justifyContent={"space-between"}>
         <Box pl={{ base: 2, md: 4 }}>
-          <StatLabel fontWeight={"medium"} isTruncated>
+          <StatLabel fontWeight={"medium"} isTruncated color={"purple.400"}>
             {title}
           </StatLabel>
           <StatNumber fontSize={"2xl"} fontWeight={"medium"}>
             {stat}
           </StatNumber>
         </Box>
-        <Box
-          my={"auto"}
-          color={useColorModeValue("gray.800", "gray.200")}
-          alignContent={"center"}
-        >
+        <Box my={"auto"} color={"purple.700"} alignContent={"center"}>
           {icon}
         </Box>
       </Flex>
@@ -45,34 +41,57 @@ function StatsCard(props) {
   );
 }
 
+//linear-gradient(to right, #8c1eaa, #272842)
 export default function BasicStatistics() {
   return (
-    <Box maxW="7xl" mx={"auto"} pt={5} px={{ base: 2, sm: 12, md: 17 }}>
+    <Flex
+      direction={"column"}
+      px={{ base: 2, sm: 12, md: 17 }}
+      justify={"center"}
+      align={"center"}
+      h={"100vh"}
+      pos={"relative"}
+    >
+      <Box
+        w={"25%"}
+        h={"25%"}
+        bgGradient={"linear(to-r,#8c1eaa,#272842)"}
+        pos={"absolute"}
+        zIndex={-3}
+        filter={"blur(100px)"}
+      ></Box>
       <chakra.h1
         textAlign={"center"}
-        fontSize={"4xl"}
+        fontSize={"6xl"}
         py={10}
         fontWeight={"bold"}
       >
-        Our company is expanding, you could be too.
+        <Text
+          bgGradient={"linear(to-r, #7928CA, #FF0080)"}
+          bgClip={"text"}
+          as={"span"}
+        >
+          비담
+        </Text>
+        과 함께라면
       </chakra.h1>
       <SimpleGrid columns={{ base: 1, md: 3 }} spacing={{ base: 5, lg: 8 }}>
         <StatsCard
-          title={"Users"}
-          stat={"5,000"}
+          title={"심리적 안정감"}
+          stat={"상승"}
           icon={<BsPerson size={"3em"} />}
         />
         <StatsCard
-          title={"Servers"}
-          stat={"1,000"}
+          title={"관리해야 할 계정"}
+          stat={"1"}
           icon={<FiServer size={"3em"} />}
         />
         <StatsCard
-          title={"Datacenters"}
+          title={"보상으로 제공하는 NFT"}
           stat={"7"}
           icon={<GoLocation size={"3em"} />}
         />
       </SimpleGrid>
-    </Box>
+    </Flex>
   );
 }
