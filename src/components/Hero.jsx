4ca9fae8b461116beb90214265aeb7d70e75d107
Baekshook <ecgo0106@naver.com>
2022-11-14 22:44:44 +0900

    onClickMintButton 구현

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,10 +8,37 @@ import {
   Image,
   Box,
 } from "@chakra-ui/react";
-
 import cardImage from "@assets/card.png";
 
 export default function CallToActionWithIllustration() {
+
+  const onClickMintButton = async () => {
+    const accounts = await window.klaytn.enable();
+    const account = accounts[0];
+    console.log(window.klaytn.networkVersion);
+    if (window.klaytn.networkVersion !== 1001) {
+      // 8217 mainnet
+      // 1001 testnet
+      alert("Warning! It is not Baobab network");
+      return;
+    }
+    const transactionParameters = {
+      to: "0x1b7967c073cc5d2b7b2a1728fbd737b567cd533f",
+      from: account,
+      data: "0xa0712d680000000000000000000000000000000000000000000000000000000000000001",
+      value: "0xB1A2BC2EC50000", //0.05 klay 단위는 klay의 가장 작은 단위 peb, 1klay = 1,000,000,000,000,000,000 peb
+      gas: "0x3476A", //무한 루프를 방지하기 위한 코드 실행의 최대 가스 허용량.
+    };
+
+    window.klaytn.sendAsync(
+      {
+        method: "klay_sendTransaction",
+        params: [transactionParameters],
+        from: account,
+      }
+    );
+  };
+
   return (
     <Container maxW={"5xl"}>
       <Stack
@@ -69,6 +96,7 @@ export default function CallToActionWithIllustration() {
             _hover={{ bg: "red.600" }}
             fontSize={{ base: "4xl", sm: "4xl", md: "4xl" }}
             fontFamily={"SEBANG_Gothic_Bold"}
+            onClick={onClickMintButton}
           >
             MINT
           </Button>
